bb4342366fb128f2cfa5341b41c19931ec955a8a
EmreHaksever <emre.haksever2@gmail.com>
2025-03-09 16:08:52 +0300

    some dependencies

---
+++ App.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, View } from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Burada sayfalarımızı ekleyeceğiz */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
